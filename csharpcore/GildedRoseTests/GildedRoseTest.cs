de21bd4e20f7d863c8dcdf9cf503de369ce7927a
Krister <kv@miles.no>
2023-04-22 11:10:19 +0200

    added first tests

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -6,13 +6,26 @@ namespace GildedRoseTests
 {
     public class GildedRoseTest
     {
+        
+        //- At the end of each day our system lowers both values for every item
         [Fact]
-        public void foo()
+        public void Item_lowers_value_at_end_of_day()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
+            IList<Item> items = new List<Item> { new() { Name = "foo", SellIn = 5, Quality = 8 },new Item { Name = "bar", SellIn = 10, Quality = 6 }  };
+            GildedRose app = new GildedRose(items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.Equal(7, items[0].Quality);
+            Assert.Equal(5, items[1].Quality);
+        }
+        
+        //- Once the sell by date has passed, Quality degrades twice as fast
+        [Fact]
+        public void Item_lowers_value_twice_as_fast()
+        {
+            IList<Item> items = new List<Item> { new() { Name = "foo", SellIn = 0, Quality = 8 } };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(6, items[0].Quality);
         }
     }
 }
