61eee02d542006022e161db9b66df6ed8475ff12
Øyvind Johannessen <oyvind@yvinds-MacBook-Pro-2.local>
2023-04-22 11:16:43 +0200

    testing

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -27,5 +27,25 @@ namespace GildedRoseTests
             app.UpdateQuality();
             Assert.Equal(6, items[0].Quality);
         }
+
+        // - The Quality of an item is never negative
+        [Fact]
+        public void Item_with_quaility_0_stays_at_0()
+        {
+            IList<Item> items = new List<Item> { new() { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(0, items[0].Quality);
+        }
+
+        // - "Aged Brie" actually increases in Quality the older it gets
+        [Fact]
+        public void Aged_brie_increases_in_quality()
+        {
+            IList<Item> items = new List<Item> { new() { Name = "Aged Brie", SellIn = 10, Quality = 5 } };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(6, items[0].Quality);
+        }
     }
 }
